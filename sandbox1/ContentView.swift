6d35ca5451291fd4aed7b4f06ff63a8ac19abe94
karyastudio <karyastudio2019@gmial.com>
2021-08-31 13:12:04 -0700

    Initial Commit

---
+++ sandbox1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sandbox1
+//
+//  Created by karyastudio on 2021-08-31.
+//  Copyright © 2021 karyastudio. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
